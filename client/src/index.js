767d5c4d2bae628e1aaf486e9f4b3772cc4fe821
FrancoMED <francoemedina@gmail.com>
2023-05-18 12:38:12 -0300

    cambio de variables src/index.js

--- client/src/index.js
+++ client/src/index.js
@@ -8,12 +8,14 @@ import reportWebVitals from "./reportWebVitals";
 import { Auth0Provider } from "@auth0/auth0-react";
 import axios from "axios";
 axios.defaults.baseURL = process.env.REACT_APP_API || "http://localhost:3001";
+const auth0Domain = process.env.REACT_APP_AUTH0_DOMAIN
+const auth0ClientId = process.env.REACT_APP_AUTH0_CLIENTID
 
 ReactDOM.render(
   <>
     <Auth0Provider
-      domain={process.env.REACT_APP_AUTH0_DOMAIN}
-      clientId={process.env.REACT_APP_AUTH0_CLIENTID}
+      domain={auth0Domain}
+      clientId={auth0ClientId}
       redirectUri={window.location.origin}
     >
       <Provider store={store}>
