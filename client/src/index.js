d014b074646b4f720234c99d772490cfc5741a2d
FrancoMED <francoemedina@gmail.com>
2023-09-07 23:24:39 -0300

    createRoot React 18

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import {createRoot} from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./Redux/Store/index";
@@ -9,22 +9,21 @@ import { Auth0Provider } from "@auth0/auth0-react";
 import axios from "axios";
 axios.defaults.baseURL = process.env.REACT_APP_API || "http://localhost:3001";
 
-ReactDOM.render(
+createRoot(document.getElementById("root")).render(
   <>
-    <Auth0Provider
-      domain={process.env.REACT_APP_AUTH0_DOMAIN}
-      clientId={process.env.REACT_APP_AUTH0_CLIENTID}
-      redirectUri={window.location.origin}
-    >
-      <Provider store={store}>
-        <Router>
-          <App />
-        </Router>
-      </Provider>
-    </Auth0Provider>
-  </>,
-  document.getElementById("root")
-);
+       <Auth0Provider
+         domain={process.env.REACT_APP_AUTH0_DOMAIN}
+         clientId={process.env.REACT_APP_AUTH0_CLIENTID}
+         redirectUri={window.location.origin}
+       >
+         <Provider store={store}>
+           <Router>
+             <App />
+           </Router>
+         </Provider>
+       </Auth0Provider>
+     </>
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
